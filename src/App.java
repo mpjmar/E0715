6f282550d5b127f54a00996703911646f7c49d42
mpjmar <meripeich@gmail.com>
2025-12-03 10:57:17 +0100

    finised

--- src/App.java
+++ src/App.java
@@ -2,38 +2,52 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int ocupacion[] = generaOcupacion(10);
-        muestraOcupacion(ocupacion);
-        int comensales = pideComensales();
-        asignaMesa(comensales, ocupacion);
+        int comensales = 0;
+        do {
+            muestraOcupacion(ocupacion);
+            comensales = pideComensales();
+            System.out.println(asignaMesa(comensales, ocupacion));
+        } while (comensales != -1);
+    }
+    
+    public static String asignaMesa(int comensales, int ocupacion[]) {
+        String mensaje = "";
+        if (comensales < -1)
+            mensaje = "El número comensales introducido no es correcto.";
+        else if (comensales == -1)
+            mensaje = "Gracias, hasta pronto.";
+        else if (comensales > 4)
+            mensaje = String.format("Lo siento, no admitimos grupos de %d, haga grupos de 4 personas como máximo e intente de nuevo.", comensales);
+        else 
+            mensaje = buscaMesa(comensales, ocupacion);
+        return mensaje;
     }
 
     public static String buscaMesa(int comensales, int ocupacion[]) {
-        boolean estaLibre = true;
-        String mensaje = "";
         int mesa = 0;
-        for (mesa = 0; mesa < ocupacion.length && !estaLibre; mesa++) {
-            if (ocupacion[mesa] + comensales <= 4)
+        while(mesa < ocupacion.length) {
+            if (ocupacion[mesa] != 0)
+                mesa++;
+            else {
+                ocupacion[mesa] += comensales;
                 return String.format("Por favor, siéntense en la mesa número %d.", mesa + 1);
-
+            }
+        }
+        mesa = 0;
+        while (mesa < ocupacion.length) {
+            if (ocupacion[mesa] + comensales <= 4) {
+                ocupacion[mesa] += comensales;
+                return String.format("Tendrán de compatir mesa. Por favor, siéntense enla mesa número %d", mesa + 1);
+            }
+            else 
+                mesa++;
         }
         return "Lo siento, en estos momentos no queda sitio.";
     }
 
-    public static void asignaMesa(int comensales, int ocupacion[]) {
-        if (comensales < -1)
-            System.out.println("El número comensales introducido no es correcto.");
-        else if (comensales == -1)
-            System.out.println("Gracias, hasta pronto.");
-        else if (comensales > 4)
-            System.out.printf("Lo siento, no admitimos grupos de %d, haga grupos de 4 personas como máximo e intente de nuevo.", comensales);
-        else {
-            buscaMesa(comensales, ocupacion);
-        }
-    }
-
     public static int pideComensales() {
         int comensales = 0;
-        do {
+        //do {
             System.out.print("¿Cuantos son? (Introduzca -1 para salir del programa): ");
             try {
                 comensales = Integer.parseInt(System.console().readLine());
@@ -44,7 +58,7 @@ public class App {
             catch (Exception e) {
                 System.out.println("Ha ocurrido un error inesperado.");
             }
-        } while (comensales == -1);
+        //} while (comensales == -1);
         return comensales;
     }
 
